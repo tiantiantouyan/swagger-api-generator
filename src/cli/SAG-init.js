1f80ca899b2c8fefc5fe9e792f5afb88d70373fc
kunsam <kunsam624@icloud.com>
2017-06-08 21:13:31 +0800

    增加初始生成文件

--- src/cli/SAG-init.js
+++ src/cli/SAG-init.js
@@ -1,11 +1,27 @@
 import commander from 'commander';
 import { version } from '../version';
+import { generateFile } from '../util/generate-file'
+import { basePath } from '../config'
+import chalk from 'chalk';
+var fs = require('fs')
+var path = require('path')
 
 const program = commander;
 
 program
   .version(version());
 
-console.log('asdsadsadsadsadsadsadasd');
+const configStr = `{\n` +
+	`\t"inputMode": "json" // "json", "url"\n` +
+	`\t"inputPath": "./swagger.json" // relative path\n` +
+	`\t"outputMode": "default", // "default", "apiOnly", "all"\n` +
+	`\t"outputPath": "./src/api" // relative path\n`  +
+`}\n`
+
+if (!fs.existsSync(path.join(basePath, './swagger-api-config'))) {
+	generateFile(configStr, basePath, 'swagger-api-config')
+	console.log(chalk.bgYellow('成功生成swagger-api-config.json配置文件'));
+}
+
 
 program.parse(process.argv);
